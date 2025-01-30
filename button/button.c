12d21c8d42fcf83a52ac682a73bfebd4f3efc79f
Steve Dunnagan <sdunnaga@redhat.com>
2025-01-30 16:46:14 -0500

    Added signal handler to GPIO button app.

--- button/button.c
+++ button/button.c
@@ -7,7 +7,6 @@
  * driver.
  * 
  * Steve Dunnagan
- * January 29, 2025
  *-----------------------------------------------------------------------------
 */
 #include <stdio.h>
@@ -16,61 +15,105 @@
 #include <fcntl.h>
 #include <string.h>
 #include <errno.h>
+#include <signal.h>
+#include <stdatomic.h>
 
 #define GPIO_BUTTON_DEVICE "/dev/gpio_button"
 #define GPIO_LED_SYSFS_PATH "/sys/class/gpio_button/gpio_button_sysfs/led_status"
 
-int main() {
-    int button_fd, led_fd;
+static volatile sig_atomic_t keep_running = 1;
+
+void sigint_handler(int sig)
+{
+    keep_running = 0;
+}
+
+int main()
+{
+    int button_fd = -1, led_fd = -1;
     char event_flag;
     char led_value[2];
     int current_led_state = 0;
+    int retval = EXIT_SUCCESS;
 
-    // Open in blocking mode
-    button_fd = open(GPIO_BUTTON_DEVICE, O_RDONLY);
-    if (button_fd < 0) {
-        perror("Failed to open GPIO button device");
-        return -1;
+    // Register SIGINT handler
+    struct sigaction sa;
+    sa.sa_handler = sigint_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    if (sigaction(SIGINT, &sa, NULL) < 0) {
+        fprintf(stderr, "Failed to register signal handler: %s\n", strerror(errno));
+        retval = EXIT_FAILURE;
+        goto cleanup;
     }
 
-    // Get initial LED state
-    led_fd = open(GPIO_LED_SYSFS_PATH, O_RDONLY);
-    if (led_fd >= 0) {
-        char init_buf[2];
-        if (read(led_fd, init_buf, sizeof(init_buf)) > 0) {
-            current_led_state = atoi(init_buf);
-        }
-        close(led_fd);
+    // Open LED sysfs ONCE (read/write mode)
+    led_fd = open(GPIO_LED_SYSFS_PATH, O_RDWR);
+    if (led_fd < 0) {
+        fprintf(stderr, "Failed to open LED sysfs: %s\n", strerror(errno));
+        retval = EXIT_FAILURE;
+        goto cleanup;
+    }
+
+    // Read initial LED state
+    if (read(led_fd, led_value, sizeof(led_value)) <= 0) {
+        fprintf(stderr, "Failed to read initial LED state: %s\n", strerror(errno));
+        retval = EXIT_FAILURE;
+        goto cleanup;
+    }
+    current_led_state = atoi(led_value);
+
+    // Open button device
+    button_fd = open(GPIO_BUTTON_DEVICE, O_RDONLY);
+    if (button_fd < 0) {
+        fprintf(stderr, "Failed to open GPIO button device: %s\n", strerror(errno));
+        retval = EXIT_FAILURE;
+        goto cleanup;
     }
 
     printf("LED Control App - Initial State: %d\n", current_led_state);
 
-    while (1) {
-        // Block until the driver signals an event via wait queue
+    while (keep_running) {
+        // Block until button event
         if (read(button_fd, &event_flag, sizeof(event_flag)) < 0) {
-            perror("Read error");
-            close(button_fd);
-            return -1;
+            if (errno == EINTR) break; // SIGINT received
+            fprintf(stderr, "Read error: %s\n", strerror(errno));
+            retval = EXIT_FAILURE;
+            goto cleanup;
         }
 
         // Toggle LED state
         current_led_state = !current_led_state;
+        snprintf(led_value, sizeof(led_value), "%d", current_led_state);
 
-        // Write new state to sysfs
-        led_fd = open(GPIO_LED_SYSFS_PATH, O_WRONLY);
-        if (led_fd < 0) {
-            fprintf(stderr, "Failed to open LED sysfs: %s\n", strerror(errno));
-            close(button_fd);
-            return -1;
+        // Reset file offset and write to LED sysfs
+        if (lseek(led_fd, 0, SEEK_SET) < 0) {
+            fprintf(stderr, "lseek failed: %s\n", strerror(errno));
+            retval = EXIT_FAILURE;
+            goto cleanup;
         }
 
-        snprintf(led_value, sizeof(led_value), "%d", current_led_state);
-        write(led_fd, led_value, 1);
-        close(led_fd);
+        if (write(led_fd, led_value, 1) < 0) {
+            fprintf(stderr, "LED write failed: %s\n", strerror(errno));
+            retval = EXIT_FAILURE;
+            goto cleanup;
+        }
 
         printf("LED Toggled → %d\n", current_led_state);
     }
 
-    close(button_fd);
-    return 0;
+cleanup:
+    printf("\nCleaning up...\n");
+
+    if (led_fd >= 0) {
+        lseek(led_fd, 0, SEEK_SET);
+        write(led_fd, "0", 1);
+        close(led_fd);
+    }
+
+    if (button_fd >= 0) {
+        close(button_fd);
+    }
+
+    return retval;
 }
